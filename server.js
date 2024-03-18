59aa8257e46b9f1f2c451ee673748ccfcd873eb3
TimL94 <99834249+TimL94@users.noreply.github.com>
2024-03-18 13:08:10 -0400

    added an async function 'askUSer' which allows the user to iterate through the same inquirer prompt

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+
 const express = require('express');
 const mysql2 = require('mysql2');
+const inquirer = require('inquirer');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -17,30 +20,41 @@ const db = mysql2.createConnection(
     console.log('Connected to company_db databse')
     );
 
-const queryOne = 'SELECT * FROM roles';
+const queryOne = 'SELECT * FROM departments';
 const queryTwo = ' SELECT r.title AS role, r.id, d.department_name AS department, r.hourly_wage FROM roles r JOIN departments d ON r.department_id = d.id;'
 const queryThree = 'SELECT e.id, e.first_name AS first, e.last_name AS last, r.title, d.department_name AS department, r.hourly_wage AS hourly, m.last_name AS manager_last_name FROM employees e LEFT JOIN employees m ON e.manager_id = m.id JOIN roles r ON e.role_id = r.id JOIN departments d ON r.department_id = d.id;'
 
-db.query(queryOne, function (err, results) {
-    if (err) {
-        console.error('error in query', err);
-    }
-    console.log(results);
-});
 
-db.query(queryTwo, function (err, results) {
-    if (err) {
-        console.error('error in query', err);
-    }
-    console.log(results);
-});
+function firstQuery() {
+    db.query(queryOne, function (err, results) {
+        if (err) {
+            console.error('error in query', err);
+        }
+        console.log(results);
+    });
+};
 
-db.query(queryThree, function (err, results) {
-    if (err) {
-        console.error('error in query', err);
-    }
-    console.log(results);
-});
+function secondQuery () {
+    db.query(queryTwo, function (err, results) {
+        if (err) {
+            console.error('error in query', err);
+        }
+        console.log(results);
+    });
+};
+
+function thirdQuery () {
+    db.query(queryThree, function (err, results) {
+        if (err) {
+            console.error('error in query', err);
+        }
+        console.log(results);
+    });
+};
+
+
+
+
 
 app.use((req,res) => {
     res.status(404).end();
@@ -49,3 +63,43 @@ app.use((req,res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
+
+
+const askUser = async () => {
+    const selection = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'selection',
+            message: 'Select an option below',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all Employees',
+                'Exit'
+            ]
+        }
+    ])
+    .then(answer => {
+        switch (answer.selection){
+            case 'View all departments':
+                firstQuery();
+                askUser();
+                break;
+            case 'View all roles':
+                secondQuery();
+                askUser();
+                break;
+            case 'View all Employees':
+                thirdQuery();
+                askUser();
+                break;
+            case 'Exit':
+                console.log('exiting program');
+                return;
+        }
+    })
+
+}
+
+
+askUser();
